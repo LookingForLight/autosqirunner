708cb50f0263c9270ee81edb4f646545c574bba0
zl29801 <zl29801@ly.com>
2019-07-14 23:04:15 +0800

    前端编码完成

--- autosqli/main/form.py
+++ autosqli/main/form.py
@@ -1,6 +1,7 @@
-#-*-coding:utf-8-*-
+# -*-coding:utf-8-*-
 from flask_wtf import FlaskForm
 from wtforms import validators, fields, widgets
+
 
 class MyForm(FlaskForm):
     Username = fields.StringField(
@@ -15,7 +16,7 @@ class MyForm(FlaskForm):
             'placeholder': '请输入用户名1',
             'required': '',
             'autofocus': ''
-            }
+        }
     )
     Password = fields.PasswordField(
         label='密码',
@@ -34,12 +35,51 @@ class MyForm(FlaskForm):
 
 
 class AutosqliForm(FlaskForm):
-
     test_url = fields.StringField(
 
         label="TEST URL:",
-        validators = [
+        validators=[
             validators.DataRequired(message='URL不能为空'),
-            validators.URL(require_tld=False,message='url不正确')
-        ]
+            validators.URL(require_tld=False, message='url不正确')
+        ],
+        widget = widgets.TextInput(),
+        render_kw={
+            "class": "form-control",
+            "placeholder": "请输入URL",
+            "required": "",
+            "autofocus": ""
+        }
+    )
+
+    request_method = fields.RadioField(
+
+        label="请求方法",
+        validators=[
+            validators.DataRequired(message='请选择请求方法'),
+        ],
+        choices=[('get', "GET"),('post','POST')],
+        default='post'
+    )
+
+    request_params = fields.TextAreaField(
+        label='请求参数',
+        validators=[
+            validators.DataRequired(message='请求参数不能为空')
+
+        ],
+        widget=widgets.TextArea(),
+        render_kw={
+            'class':'form-control',
+            'required':'',
+            'rows':6
+        }
+    )
+
+    ignore_params = fields.StringField(
+        label='忽略注入字段',
+        widget=widgets.TextInput(),
+        render_kw={
+                'class':'form-control',
+                'placeholder':'输入不要检测的字段名'
+        }
     )
--- autosqli/main/view.py
+++ autosqli/main/view.py
@@ -1,6 +1,6 @@
 #-*-coding:utf-8-*-
 from flask import Flask, render_template, url_for, request, redirect
-from .form import MyForm
+from .form import MyForm,AutosqliForm
 from . import sqil
 
 @sqil.route('/login', methods=['GET', 'POST'])
@@ -19,3 +19,23 @@ def login():
 @sqil.route('/')
 def index():
     return render_template('index.html')
+
+@sqil.route('/home',methods=['GET', 'POST'])
+def autosqli():
+
+    if request.method == 'GET':
+        form = AutosqliForm()
+    if request.method == 'POST':
+        form = AutosqliForm(request.form)
+        if form.validate_on_submit():
+            url = form.test_url.data
+            method = form.request_method.data
+            request_params = form.request_params.data
+            ignore_params = form.ignore_params.data
+
+            send_params = "url : {0},\nmethod :{1},\nrequest_params :{2},\nignore: {3}".format(url,method,request_params,ignore_params)
+            print(send_params)
+            return redirect('http://www.baidu.com')
+        else:
+            print(form.errors)
+    return render_template('autosqli/index.html',form = form)
